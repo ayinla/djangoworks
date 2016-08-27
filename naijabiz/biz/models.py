208a34e44853c80de8f124967356d21a03293227
india mehdi <india@fedora.ayinla.com>
2016-08-27 19:24:43 -0400

    changes

--- naijabiz/biz/models.py
+++ naijabiz/biz/models.py
@@ -7,5 +7,6 @@ class Biz(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=200)
     city = models.CharField(max_length=200)
+    state = models.CharField(max_length=20)
     biztype= models.CharField(max_length=20)
     telephone = models.CharField(max_length=10)
